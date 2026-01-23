a1c88c543c967b514da937ec5e9069f261130aeb
Sasanka Bandara <91615202+Sasankamadura@users.noreply.github.com>
2026-01-24 00:19:25 +0530

    Revert "Add Slim Fast Hybrid encoder and config for RT-DETR"
    This reverts commit abcbf871d7d3ed1e9b378f0ed4dca4f6ca263abd.

--- rtdetr_pytorch/src/zoo/rtdetr/__init__.py
+++ rtdetr_pytorch/src/zoo/rtdetr/__init__.py
@@ -5,7 +5,6 @@
 from .rtdetr import *
 
 from .hybrid_encoder import *
-from .hybrid_encoder_slim_fast import *
 from .rtdetr_decoder import *
 from .rtdetr_postprocessor import *
 from .rtdetr_criterion import *
